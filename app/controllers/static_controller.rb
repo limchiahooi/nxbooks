6a7c0e024b3f029598e1430223d69365cd4ba0e4
limchiahooi <limchiahooi@gmail.com>
2018-02-02 01:03:49 +0800

    fixed typo

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -15,14 +15,9 @@ class StaticController < ApplicationController
 		@quote = hash["quote"]
 		@author = hash["author"]
 		@cat = hash["cat"]
-
-		resp3 = RestClient.get("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1")
+		
+		resp3 = RestClient.get("http://api.icndb.com/jokes/random")
 		hash = JSON.parse(resp3.body)
-		@title = hash[0]["title"]
-		@content = hash[0]["content"]
-
-		resp4 = RestClient.get("http://api.icndb.com/jokes/random")
-		hash = JSON.parse(resp4.body)
 		@joke = hash["value"]["joke"]
 		
 
